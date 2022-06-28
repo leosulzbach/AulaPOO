1e2f9d2cb103378d5683160f4bb39213dd02f28f
Leonardo Luís Sulzbach <leonardo.sulzbach1@universo.univates.br>
2022-06-28 12:56:45 -0300

    Create Teste.java

---
+++ POO/src/modelos/Teste.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Teste {
+    
+    private String cu;
+    private int nCus;
+
+    public Teste(String cu, int nCus) {
+        this.cu = cu;
+        this.nCus = nCus;
+    }
+
+    public String getCu() {
+        return cu;
+    }
+
+    public void setCu(String cu) {
+        this.cu = cu;
+    }
+
+    public int getnCus() {
+        return nCus;
+    }
+
+    public void setnCus(int nCus) {
+        this.nCus = nCus;
+    }
+    
+    
+    
+    
+}
