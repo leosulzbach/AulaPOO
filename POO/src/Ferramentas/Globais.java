62233df3b8cea2b03185f6ec556ddfc3e2972018
Leonardo Luís Sulzbach <leonardo.sulzbach1@universo.univates.br>
2022-06-29 13:44:22 -0300

    Update Globais.java

--- POO/src/Ferramentas/Globais.java
+++ POO/src/Ferramentas/Globais.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Ferramentas;
-
+import java.util.Random;
 /**
  *
  * @author Windows 10
  */
 public class Globais {
-    
+    public static int gerarNumero(int max) {
+        try {
+
+            Random rand = new Random(); //instance of random class
+            int upperbound = max;
+            //generate random values from 0-max
+            int int_random = rand.nextInt(upperbound);
+            
+            return int_random + 1;
+
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
 }
